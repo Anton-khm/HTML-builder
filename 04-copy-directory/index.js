1da75fbb2dce4637d4e83e8b00e2731a60f6be8f
Anton-khm <anton.khmialeuski@gmail.com>
2025-01-20 23:09:16 +0300

    refactor: add copyDir function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,25 +4,30 @@ const path = require("path");
 const existingPath = path.join(__dirname, "files");
 const newPath = path.join(__dirname, "files-copy");
 
-fs.mkdir(newPath, { recursive: true }, (err) => {
-    if (err) throw err;
-});
+function copyDir(current, newf) {
+    fs.mkdir(newf, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
 
-fs.readdir(existingPath, { withFileTypes: true }, (err, files) => {
-    if (err) {
-        console.log(err);
-    } else {
-        files.forEach(file => {
-            if (file.isFile()) {
-                fs.copyFile(path.join(existingPath, file.name), path.join(newPath, file.name), (err) => {
-                    if (err) {
-                        console.log("Error:", err);
-                    }
-                    else {
-                        console.log(`${file.name} copied`);
-                    }
-                })
-            }
-        })
-    }
-});
+    fs.readdir(current, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            files.forEach(file => {
+                if (file.isFile()) {
+                    fs.copyFile(path.join(current, file.name), path.join(newf, file.name), (err) => {
+                        if (err) {
+                            console.log("Error:", err);
+                        }
+                        else {
+                            console.log(`${file.name} copied`);
+                        }
+                    })
+                }
+            })
+        }
+    });
+
+}
+
+copyDir(existingPath, newPath)
